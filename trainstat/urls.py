140f2f35e2f9a5ee2552a3f83f63dfa4e31e222b
yatingupta10 <yatingupta12@gmail.com>
2016-04-03 01:24:21 +0530

    modified frontend

--- trainstat/urls.py
+++ trainstat/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^contact.html$', views.contact, name='contact'),
+    url(r'^about.html$', views.contact, name='about'),
 ]
--- trainstat/views.py
+++ trainstat/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'trainstat/index.html', {})
+
+def contact(request):
+    return render(request, 'trainstat/contact.html', {})
+
+def about(request):
+    return render(request, 'trainstat/about.html', {})
 # Create your views here.

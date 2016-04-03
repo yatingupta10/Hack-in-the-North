7a546596cbbf64bd1704b8c4cf62c66bbdb84379
yatingupta10 <yatingupta12@gmail.com>
2016-04-03 09:45:27 +0530

    added feedback form

--- trainstat/urls.py
+++ trainstat/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 from . import views
 
+
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^contact.html$', views.contact, name='contact'),
     url(r'^about.html$', views.about, name='about'),
-    url(r'^rate.html$', views.about, name='rate'),
+    url(r'^rate.html$', views.rate, name='rate'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
 
 ]
--- trainstat/views.py
+++ trainstat/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'trainstat/index.html', {})
 
@@ -11,5 +12,18 @@ def about(request):
 
 def rate(request):
     return render(request, 'trainstat/rate.html', {})
+
+def post_new(request):
+    if request.method == "POST":
+        saved_no = request.POST.get("No", "")
+        saved_punctuality = request.POST.get("Punctuality", "")
+        saved_crowd = request.POST.get("Crowd", "")
+        saved_services = request.POST.get("Services", "")
+        saved_cooling = request.POST.get("Cooling", "")
+        saved_seatavailability = request.POST.get("SeatAvailability", "")
+        Punctuality = models.FloatField()
+        feedback = Feedback()
+        feedback.save()
+    return HttpResponseRedirect(reverse("view", args=(Review.id,)))
 
 # Create your views here.
